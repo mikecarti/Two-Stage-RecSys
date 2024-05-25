8252cb8e97630017c36a1229a64cf9e8b31027f6
Daniel Timchenko <mikecartibeats@gmail.com>
2024-05-25 18:55:15 -0400

    fastapi

--- infer.py
+++ infer.py
@@ -1,17 +1,41 @@
-from typing import Iterable
-
+from typing import List
+from pydantic import BaseModel
 import numpy as np
-
+from fastapi import FastAPI
 from train import train
+
+
+class PredictRequest(BaseModel):
+    user_ids: np.array
+
+
+class PredictResponse(BaseModel):
+    predictions: np.ndarray
 
 
 class Inference:
     def __init__(self):
         self.model = train()
 
-    def predict(self, user_ids: Iterable) -> np.ndarray:
+    def predict(self, user_ids: np.ndarray) -> np.ndarray:
         """
         :param user_ids: np.array of dimension 1
         :return: np.ndarray of dimension len(user_ids) x len(item_ids
         """
         return self.model.predict(user_ids)
+
+
+inference = Inference()
+app = FastAPI()
+
+
+@app.post("/predict", response_model=PredictResponse)
+def predict(request: PredictRequest):
+    predictions = inference.predict(np.array(request.user_ids))
+    return PredictResponse(predictions=predictions)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
